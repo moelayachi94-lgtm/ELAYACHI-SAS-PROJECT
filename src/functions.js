13d7edb4bcc0bb0b464331e5656a6f7e18891f4f
Mohammed El Aayachi <moelayachi94@gmail.com>
2026-09-17 17:12:05 +0100

    first function finish

--- src/functions.js
+++ src/functions.js
@@ -4,7 +4,7 @@ export function normaliserNom(nom) {
     return nom.trim().toLowerCase();
 }
 
-export function validerResultat(jour, exercicesTermines, totalExercices) {
+export function validerResultat(jour, exercicesTermines, totalExercices,) {
     if (jour < 1 || jour > 7) {
         return false;
     }
@@ -13,5 +13,23 @@ export function validerResultat(jour, exercicesTermines, totalExercices) {
         return false;
     }
 
+    if ( exercicesTermines < 0 || totalExercices < 0){
+        return false; 
+    }
     return true;
+}
+
+export function ajouterApprenant ( nomComplet, ville){
+ for ( let i of apprenants)
+    if (nomComplet === i.nomComplet)
+        return false ; 
+
+let newStudent = {
+    id : apprenants.length + 1, 
+    nomcomplet : nomComplet,
+    ville : ville,
+    resultats : []
+}
+apprenants.push(newStudent)
+        return true 
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-import { normaliserNom, validerResultat } from "./functions.js";
+import { ajouterApprenant } from "./functions.js";
 
-console.log(normaliserNom("   SARA DEV   "));
-
-console.log(validerResultat(3, 15, 20));
-console.log(validerResultat(8, 15, 20));
-console.log(validerResultat(3, 25, 20));
+console.log(ajouterApprenant("logos","oujda"));
